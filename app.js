bb439270c7e8f1acdd093dc43b33d5d10074a277
u4su06 <doremi90932@gmail.com>
2020-12-30 19:18:45 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
-const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser') // 引用 body-parser
+
 const Record = require('./models/record.js') // 載入 record model
 
 const app = express()
@@ -21,12 +23,34 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // 設定首頁路由
 app.get('/', (req, res) => {
   Record.find() // 取出 record model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(records => res.render('index', { records })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
+})
+
+// new 頁面
+app.get('/records/new', (req, res) => {
+  return res.render('new')
+})
+
+//  Create 動作
+app.post('/records', (req, res) => {
+  const newItem = req.body       // 從 req.body 拿出表單裡的 name 資料
+
+  return Record.create({  // 存入資料庫
+    name: newItem.name,
+    date: newItem.date,
+    category: newItem.category,
+    amount: newItem.amount,
+  })
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
 })
 
 // 設定 port 3000
