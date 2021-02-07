f4935fd88012e6741e8107a476b116d923759bbb
u4su06 <doremi90932@gmail.com>
2021-02-07 10:23:27 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
+// 如果在 Heroku 環境則使用 process.env.PORT
+// 否則為本地環境，使用 3000 
+const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -20,6 +23,6 @@ app.use(methodOverride('_method'))
 app.use(routes)
 
 // 設定 port 3000
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 })
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,11 @@
 // 載入 mongoose
 const mongoose = require('mongoose')
+// 如果在 Heroku 環境則使用 process.env.MONGODB_URI
+// 否則為本地環境，使用 mongodb://localhost/todo-list
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/Expense'
+
 // 設定連線到 mongoDB
-mongoose.connect('mongodb://localhost/Expense', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
